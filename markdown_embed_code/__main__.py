71edd9bd83929a768d9f92aa1d1f7fb3f8f1742c
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2023-08-03 19:01:58 +0200

    chore: add some debug ingo

--- markdown_embed_code/__main__.py
+++ markdown_embed_code/__main__.py
@@ -36,7 +36,12 @@ settings = Settings()
 
 subprocess.run(['pwd'])
 subprocess.run(['ls', '-la'])
-subprocess.run(["git", "config", "--local", "user.name", "github-actions"], check=True)
+subprocess.run(['ls', '-la', '.git'])
+subprocess.run(['cat', '.git/config'])
+subprocess.run(['git', 'status'])
+subprocess.run(['git', 'config', '-l'])
+subprocess.run(['git', 'config', '--local', '-l'])
+subprocess.run(["git", "config", "--local", "user.name", "github-actions" "--verbose"], check=True)
 subprocess.run(
     ["git", "config", "--local", "user.email", "github-actions@github.com"], check=True
 )
